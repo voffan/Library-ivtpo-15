2234ecae4031fc8ec2ad8b014004b83344889324
Aysen93 <frikazzoed@mail.ru>
2018-03-27 13:18:43 +0900

    Added Publisher class

---
+++ Library/Library/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Publisher
+    {
+        private string name;
+        public Publisher() { }
+        public Publisher(string status_name)
+        private City city;
+    }
+}
