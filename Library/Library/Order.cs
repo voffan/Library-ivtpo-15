23ffeca2fd76a4fff817113f0f531ccc9deb7cc9
bokunoheya <lelong97@mail.ru>
2018-03-27 13:12:23 +0900

    Added Order class

---
+++ Library/Library/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Order
+    {
+        private DateTime orderdate;
+        private DateTime returndate;
+        private Boolean status;
+        public Order () { }
+        public Order (DateTime orderdate, DateTime returndate, Boolean status)
+        {
+
+        }
+
+    }
+}
