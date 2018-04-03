4907ea9cd5054dcdc3e22916960181f5a8542dde
Marik666 <Msmark@mail.ru>
2018-04-03 12:18:56 +0900

    Added Lending

---
+++ Library/Library/Lending.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+        public class Lending
+        {
+            private string name;
+            private library lib;
+            public Lending() { }
+            public Lending(string status_name, library lib)
+            {
+                name = status_name;
+                this.lib = lib;
+            }
+            public string Name { get { return name; } set { name = value; } }
+            public library LIBRARY { get { return lib; } }
+        }
+}
