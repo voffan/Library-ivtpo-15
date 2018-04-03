5c93a069a2e6157ce7ba62066e4b554bec69873c
Marik666 <Msmark@mail.ru>
2018-04-03 13:32:05 +0900

    Added Orderbooks

---
+++ Library/Library/Orderbooks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Orderbooks
+    {
+        private Book;
+        private int;
+        private BookStatus status;
+        public Orderbooks() { }
+        public Orderbooks(Book book, int count, BookStatus status)
+        {
+            this.book = book;
+            this.count = count;
+            this.status = status;
+        }
+        public book  { get { return Book; } set { orderdate = value; } }
+        public count { get { return int; } set { returndate = value; } }
+        public status { get { return BookStatus; } set { status = value; } }
+    }
+}
+}
