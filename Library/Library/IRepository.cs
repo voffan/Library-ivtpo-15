45d49ff5b440e372a5a386c6f09668d1ae3f0718
voffan <vovan_evv@rambler.ru>
2018-04-10 12:31:27 +0900

    Added IRepository.cs

---
+++ Library/Library/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public interface IRepository<T>:IDisposable
+    {
+        IEnumerable<T> GetObjects();
+        T GetObjectByID(int entityId);
+        void InsertObject(T entity);
+        void DeleteObject(int enitityId);
+        void UpdateObject(T entity);
+        void Save();
+    }
+}
