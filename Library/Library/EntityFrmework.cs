8f11d80f9df2900793725b8af0c5b9dfef779cc1
voffan <vovan_evv@rambler.ru>
2018-04-10 17:28:08 +0900

    Добавил UserRepository

--- Library/Library/EntityFrmework.cs
+++ Library/Library/EntityFrmework.cs
@@ -1,20 +1,24 @@
-﻿public class ProductContext : DbContext
+﻿using System.Data.Entity;
+
+namespace Library
 {
-    public DbSet<Book> Book { get; set; }
-    public DbSet<Author> Author { get; set; }
-    public DbSet<BookStatus> BookStatuss { get; set; }
-    public DbSet<city> city { get; set; }
-    public DbSet<country> country { get; set; }
-    public DbSet<Lending> Lending { get; set; }
-    public DbSet<library> library { get; set; }
-    public DbSet<ms> ms { get; set; }
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<BookStatus> BookStatuss { get; set; }
+        public DbSet<City> city { get; set; }
+        public DbSet<Country> country { get; set; }
+        public DbSet<Lending> Lending { get; set; }
+        public DbSet<library> library { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
-    public DbSet<Order> Order { get; set; }
-    public DbSet<Position> Position { get; set; }
-    public DbSet<Program> Program { get; set; }
-    public DbSet<Publisher> Publisher { get; set; }
-    public DbSet<Reader> Reader { get; set; }
-    public DbSet<ReadengRoom> ReadengRoom { get; set; }
-    public DbSet<User> User { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Position> Position { get; set; }
+        public DbSet<Publisher> Publisher { get; set; }
+        public DbSet<Reader> Reader { get; set; }
+        public DbSet<ReadingRoom> ReadengRoom { get; set; }
+        public DbSet<User> User { get; set; }
 
+    }
 }
--- Library/Library/User.cs
+++ Library/Library/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Library
 {
@@ -18,5 +19,62 @@ namespace Library
         public string Login { get { return name; } }
         public string Password { get { return pwd; } }
     }
-}
 
+    public class UserRepository:IRepository<User>, IDisposable
+    {
+        private LibraryContext context;
+
+        private bool disposed = false;
+
+        public UserRepository(LibraryContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<User> GetObjects() 
+        {
+            return context.User.ToList();
+        }
+        User GetObjectByID(int entityId) 
+        {
+            return context.User.Find(entityId);
+        }
+        void InsertObject(User entity) 
+        {
+            context.User.Add(entity);
+        }
+        void DeleteObject(int entityId) 
+        {
+            User u = context.User.Find(entityId);
+            context.User.Remove(u);
+        }
+        void UpdateObject(User entity) 
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        void Save() 
+        {
+            context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
