d5e2377a22a5eaaea376da8a02f53d951d09d592
Marik666 <Msmark@mail.ru>
2018-04-24 12:16:32 +0900

    ReadingRoom

--- Library/Library/ReadingRoom.cs
+++ Library/Library/ReadingRoom.cs
@@ -9,11 +9,68 @@ namespace Library
     public class ReadingRoom
     {
         public ReadingRoom() { }
-        public ReadingRoom(string room_name)
+        public ReadingRoom(string name)
         {
-            Name = room_name;
+            Name = name;
         }
         public int ReadingRoomID { get; set; }
         public string Name { get; set; }
+    }
+    public class ReadingRoomRepository : IRepository<ReadingRoom>, IDisposable
+    {
+        private LibraryContext context;
+
+        private bool disposed = false;
+
+        public LendingRepository(LibraryContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<ReadingRoom> GetObjects()
+        {
+            return context.ReadingRoom.ToList();
+        }
+        public ReadingRoom GetObjectByID(int entityId)
+        {
+            return context.ReadingRoom.Find(entityId);
+        }
+        public void InsertObject(ReadingRoom entity)
+        {
+            context.Lendings.Add(entity);
+        }
+        public void DeleteObject(int entityId)
+        {
+            ReadingRoom u = context.Lendings.Find(entityId);
+            context.Lendings.Remove(u);
+        }
+        public void UpdateObject(ReadingRoom entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
